fa55cc5655c62c34e9c83f16403834dc09bfb4b6
anna-moore <ammoore434@gmail.com>
2020-11-20 11:23:29 -0600

    field.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,18 @@
+import { addPlant, usePlant } from './field.js';
 import { createPlan } from './plan.js'
-import { createAsparagus } from "./seeds/asparagus.js"
+import { createAsparagus } from './seeds/asparagus.js'
 
+//test harvest plan
 const yearlyPlan= createPlan();
 console.log(yearlyPlan);
 
-
+//test creating the seeds
 const asparagusSeed = createAsparagus();
 console.log(asparagusSeed);
+
+//test tilling the fiels
+let seed = [];
+seed = createAsparagus();
+const test = addPlant(seed);
+const test2 = usePlant();
+console.log(test, test2);
